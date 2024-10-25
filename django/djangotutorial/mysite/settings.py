2365a7f3e6617156ffe3d5e2c2a8a785425222c5
Nguyễn Tuấn Minh (Pigeon) <93970330+p1geonium@users.noreply.github.com>
2024-10-25 04:45:28 +0000

    aaa

--- django/djangotutorial/mysite/settings.py
+++ django/djangotutorial/mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
+    # 'import_export',
 ]
 
 MIDDLEWARE = [
